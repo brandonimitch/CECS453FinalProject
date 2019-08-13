38f575f0a7a1c7047aa40ae6f5dc34d78d4c83f4
Brandon Mitchell <brandonmitchell938@yahoo.com>
2019-08-13 09:50:33 -0700

    fragment transaction Login Signup

--- app/src/main/java/com/example/cecs453finalproject/Signup.java
+++ app/src/main/java/com/example/cecs453finalproject/Signup.java
@@ -4,9 +4,11 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
@@ -64,7 +66,19 @@ public class Signup extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_signup, container, false);
+        View v = inflater.inflate(R.layout.fragment_signup, container, false);
+
+        Button signup = v.findViewById(R.id.signupBtnSignup);
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fragmentManager = getFragmentManager();
+                fragmentManager.beginTransaction().replace(R.id.mainContentFrameContainer, new Login()).commit();
+            }
+        });
+
+        return v;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
