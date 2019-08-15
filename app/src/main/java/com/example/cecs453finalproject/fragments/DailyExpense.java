f4d045123f4b4a585e9e1238a9f4ba5be17626e6
Brandon Mitchell <brandonmitchell938@yahoo.com>
2019-08-14 21:52:59 -0700

    database logic Add / Edit DailyExpense fragment

--- app/src/main/java/com/example/cecs453finalproject/fragments/DailyExpense.java
+++ app/src/main/java/com/example/cecs453finalproject/fragments/DailyExpense.java
@@ -14,10 +14,13 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
+import com.example.cecs453finalproject.MainActivity;
 import com.example.cecs453finalproject.R;
 import com.example.cecs453finalproject.adapters.CategorySpinnerAdapter;
 import com.example.cecs453finalproject.adapters.MyRecyclerViewAdapter;
+import com.example.cecs453finalproject.classes.Category;
 import com.example.cecs453finalproject.classes.Transaction;
 import com.example.cecs453finalproject.database.CategoryDAO;
 import com.example.cecs453finalproject.database.TransactionDAO;
@@ -35,8 +38,9 @@ import java.util.List;
  * Use the {@link DailyExpense#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class DailyExpense extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
+public class DailyExpense extends Fragment implements MyRecyclerViewAdapter.ItemClickListener {
+
+
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String USER_ID = "param1";
     private static final String USERNAME = "param2";
@@ -56,12 +60,16 @@ public class DailyExpense extends Fragment {
     private Spinner spinner;
     private String categoryNameEntered;
     private double expenseAmountEntered;
+
     private RecyclerView mItemsList;
     private List<Transaction> mTransactionList;
+    private TransactionDAO mTransactionDAO;
+    private UsersDAO mUserDAO;
+    private CategoryDAO mCategoryDAO;
+    private List<Category> mCategoryList;
+    private DailyExpense.OnFragmentInteractionListener mListener;
 
 
-
-    private OnFragmentInteractionListener mListener;
 
     public DailyExpense() {
         // Required empty public constructor
@@ -76,10 +84,10 @@ public class DailyExpense extends Fragment {
      * @return A new instance of fragment DailyExpense.
      */
     // TODO: Rename and change types and number of parameters
-    public static DailyExpense newInstance(String param1, String param2) {
+    public static DailyExpense newInstance(Long param1, String param2) {
         DailyExpense fragment = new DailyExpense();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
+        args.putLong(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
@@ -88,57 +96,82 @@ public class DailyExpense extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mUserID = getArguments().getLong(USER_ID);
-            mUsername = getArguments().getString(USERNAME);
-        }
+//        if (getArguments() != null) {
+//            mUserID = getArguments().getLong(USER_ID);
+//            mUsername = getArguments().getString(USERNAME);
+//        }
 
-//        mTransactionDAO = new TransactionDAO(getActivity());
-//        mUserDAO = new UsersDAO(getActivity());
-//        mCategoryDAO = new CategoryDAO(getActivity());
-//        mTransactionList = mTransactionDAO.getUserTransactions(mUserID);
-//        mCategoryList = mCategoryDAO.getUserCategories(mUserID);
+        mUserID = ((MainActivity) getActivity()).getLoggedInUserId();
+        mUsername = ((MainActivity) getActivity()).getLoggedInUsername();
+
+
+        mTransactionDAO = new TransactionDAO(getActivity());
+        mUserDAO = new UsersDAO(getActivity());
+        mCategoryDAO = new CategoryDAO(getActivity());
+        mTransactionList = mTransactionDAO.getUserTransactions(mUserID);
+        mCategoryList = mCategoryDAO.getUserCategories(mUserID);
     }
 
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_daily_expense, container, false);
+        View expenseView = inflater.inflate(R.layout.fragment_expenses, container, false);
 
         // Define control objects.
+        dailyExpenseBtn = v.findViewById(R.id.daliyExpenseBtn);
         categoryName = v.findViewById(R.id.dailyExpenseEditText1);
         expenseAmount = v.findViewById(R.id.dailyExpenseEditText2);
-        dailyExpenseBtn = v.findViewById(R.id.dailyExpenseBtn);
         spinner = v.findViewById(R.id.dailyExpenseSpinner);
 
 
-        mItemsList = v.findViewById(R.id.expense_recycler_view);
+        mItemsList = expenseView.findViewById(R.id.expense_recycler_view);
         mItemsList.setLayoutManager(new LinearLayoutManager(getActivity()));
-//        final MyRecyclerViewAdapter adapter = new MyRecyclerViewAdapter(this, mTransactionList,
-//                mCategoryList,this);
+        final MyRecyclerViewAdapter adapter = new MyRecyclerViewAdapter(this, mTransactionList,
+                mCategoryList,this);
 
-//        mItemsList.setAdapter(adapter);
-
-//        //TODO: DELETE AFTER TESTING IS COMPLETE
-//        Transaction newTransaction = mTransactionDAO.createTransaction(mUserID,"7/7/2019",
-//                "Test Expense","Category", -1, 100.00);
-//        mTransactionList.add(newTransaction);
-//
-//        mTransactionDAO.deleteAllUserTransactions(mUserID);
-
-
+        mItemsList.setAdapter(adapter);
 
         dailyExpenseBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                categoryNameEntered = categoryName.getText().toString();
-                expenseAmountEntered = Double.parseDouble(expenseAmount.getText().toString());
+                Context context = getActivity().getApplicationContext();
 
-                ///// TODO: ADD FUNCTIONALITY TO EDIT DAILY EXPENSE AMOUNT IN DATABASE ///////
+                if (expenseAmount != null && categoryName != null) {
+
+                    if (categoryName.getText().toString().length() != 0) {
+
+                        categoryNameEntered = categoryName.getText().toString();
+
+                    } else if (categoryName.getText().toString().length() == 0) {
+
+                        Toast.makeText(context, "You must enter a category!", Toast.LENGTH_SHORT).show();
+
+                    } else if (expenseAmount.getText().toString().length() != 0) {
+
+                        expenseAmountEntered = Double.parseDouble(expenseAmount.getText().toString());
+
+                    } else if (expenseAmount.getText().toString().length() == 0) {
+
+                        Toast.makeText(context, "You must enter an amount in dollars!", Toast.LENGTH_SHORT).show();
+
+                    } else {
+
+                        // Database Transaction: add values to database.
+                        Transaction newTransaction0 = mTransactionDAO.createTransaction(mUserID,
+                                "7/10/2019", "New / Edit Expense", categoryNameEntered, -1, expenseAmountEntered);
+                        Toast.makeText(context, "Database updated " + categoryNameEntered + " with $" + expenseAmountEntered, Toast.LENGTH_SHORT).show();
+
+                    }
+
+                } else {
+
+                    Toast.makeText(context, "You must enter both category name and expense amount!", Toast.LENGTH_SHORT).show();
+                }
 
             }
         });
@@ -170,6 +203,11 @@ public class DailyExpense extends Fragment {
     public void onDetach() {
         super.onDetach();
         mListener = null;
+    }
+
+    @Override
+    public void onItemClick(View view, int position) {
+
     }
 
     /**
--- app/src/main/java/com/example/cecs453finalproject/fragments/Expenses.java
+++ app/src/main/java/com/example/cecs453finalproject/fragments/Expenses.java
@@ -90,15 +90,9 @@ public class Expenses extends Fragment implements MyRecyclerViewAdapter.ItemClic
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_expenses, container, false);
-
-        if (savedInstanceState != null) {
-
-            // Obtain username and customer ID from main activity.
-            String username = getArguments().getString("username");
-            Long customerId = getArguments().getLong("customerID");
-        }
 
 
         mItemsList = (RecyclerView) v.findViewById(R.id.expense_recycler_view);
